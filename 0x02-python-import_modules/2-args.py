36086fc32a0c151a6dbebce28eea691b6f3201a0
safi222 <safia2000gibril@gmail.com>
2024-01-18 03:40:24 -0800

    0x02. Python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from sys import argv
+    """Print the number of and list of arguments."""
+    import sys
 
-    argstr = "arguments"
-    argc = len(argv) - 1
-    mark = ":"
-
-    if (argc == 1):
-        argstr = "argument"
-    if (argc == 0):
-        mark = "."
-
-    print("{} {}{}".format(argc, argstr, mark))
-
-    for arg in range(1, argc + 1):
-        print("{}: {}".format(arg, argv[arg]))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from sys import argv
+    """Print the addition of all arguments."""
+    import sys
 
-    sum = 0
-    for i in range(1, len(argv)):
-        sum += int(argv[i])
-
-    print(sum)
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
