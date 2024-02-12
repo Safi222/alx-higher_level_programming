7c05954cb8b54038bfd61dd57acb3521c88d9540
safi222 <safia2000gibril@gmail.com>
2024-02-11 20:45:32 -0800

    0x05-python-exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def safe_function(fct, *args):
-        """Execute a function safely."""
+    """Execute a function safely."""
 
-            from sys import stderr
+    from sys import stderr
 
-                try:
-                    result = fct(*args)
-                    return (result)
-                except Exception as e:
-                    print("Exception: {}".format(e), file=stderr)
-                    return (None)
+    try:
+        result = fct(*args)
+        return (result)
+    except Exception as e:
+        print("Exception: {}".format(e), file=stderr)
+        return (None)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,15 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-        """Print x integers from the list."""
+    """Print x integers from the list."""
 
-        count = 0
-        for i in range(0, x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-            except (TypeError, ValueError):
-                continue
-            else:
-                count += 1
-            print()
-            return (count)
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (TypeError, ValueError):
+            continue
+        else:
+            count += 1
+    print()
+    return (count)
